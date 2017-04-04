105133d8c6112968847df226b6944467190fb3fb
Walter <relw1wa-github@yahoo.de>
2017-04-04 17:28:21 +0200

    Implement responsive SimonControls and switch from CDN to NPM integration of Foundation

--- src/components/simon-board.js
+++ src/components/simon-board.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import SimonControls from './simon-controls';
 import SimonFields from './simon-fields';
 
-const SimonBoard = (props) => {
-  return (
-    <div className="simon-board row text-center">
-      <SimonFields />
-      <SimonControls />
-    </div>
-  );
+class SimonBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTurnedOn: false
+    }
+  }
+
+  handleToggleTurnedOn() {
+    this.setState({
+      isTurnedOn: !this.state.isTurnedOn
+    });
+  }
+
+  render() {
+    const { isTurnedOn } = this.state;
+
+    return (
+      <div className="simon-board row text-center">
+        <SimonFields />
+        <SimonControls
+          isTurnedOn={isTurnedOn}
+          onToggleTurnedOn={() => this.handleToggleTurnedOn()}
+        />
+      </div>
+    );
+  }
 };
 
 export default SimonBoard;
--- src/components/simon-controls.js
+++ src/components/simon-controls.js
@@ -1,12 +1,72 @@
 import React from 'react';
 
 const SimonControls = (props) => {
+  const { isTurnedOn, onStartOrRestart, onToggleStrictMode, onToggleTurnedOn } = props;
 
   return (
-    <div className="simon-controls column small-5 small-offset-1 text-center">
+    <div className="simon-controls column small-7 medium-5 medium-offset-1 text-center">
       <div className="row">
-        <div id="simon-options" className="column small-12">
-          <div>
+        <div className="simon-controls-wrapper-1 column small-12">
+          <div className="simon-controls-wrapper-2">
+
+            <div className="simon-controls-top">
+              <h3>Simon<sup className="small">&reg;</sup></h3>
+            </div>
+
+            <div className="simon-controls-middle">
+
+              <div className="simon-controls-middle-1 row collapse">
+                <div className="strict-mode-indicator column small-offset-9 small-2 text-center">
+                  <span className=""></span>
+                </div>
+              </div>
+
+              <div className="simon-controls-middle-2 row collapse">
+                <div className="column small-4">
+                  {/* was not able to position span in accordance with the other buttons in
+                      this row without using button :-( */}
+                  <button className="turn-counter-button">
+                    <span className="turn-counter">00</span>
+                  </button>
+                </div>
+                <div className="column small-4">
+                  <button
+                    className="re-start-button"
+                    onClick={onStartOrRestart}
+                  ></button>
+                </div>
+                <div className="column small-4">
+                  <button
+                    className="strict-mode-toggle"
+                    onClick={onToggleStrictMode}></button>
+                </div>
+              </div>
+
+              <div className="simon-controls-middle-3 row collapse">
+                <div className="column small-4">
+                  <div className="simon-controls-text">Count</div>
+                </div>
+                <div className="column small-4">
+                  <div className="simon-controls-text">Start</div>
+                </div>
+                <div className="column small-4">
+                  <div className="simon-controls-text">Strict</div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="simon-controls-bottom">
+              <div className="switch tiny" onClick={onToggleTurnedOn} >
+                <input className="switch-input" checked={isTurnedOn} id="exampleSwitch" type="checkbox" name="exampleSwitch" />
+                <label className="switch-paddle" for="exampleSwitch">
+                  <span className="show-for-sr">Turn Simon Game on or off</span>
+                  <span className="switch-active" aria-hidden="true">On</span>
+                  <span className="switch-inactive" aria-hidden="true">Off</span>
+                </label>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
@@ -15,6 +75,3 @@ const SimonControls = (props) => {
 };
 
 export default SimonControls;
-
-
-
