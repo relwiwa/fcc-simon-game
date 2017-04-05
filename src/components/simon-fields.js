0b42a88bad500f7fc52279d64454cfd88f7cc8a0
Walter <relw1wa-github@yahoo.de>
2017-04-05 11:20:18 +0200

    Implement part of the game functionality

--- src/components/simon-fields.js
+++ src/components/simon-fields.js
@@ -3,23 +3,27 @@ import React from 'react';
 import SimonField from './simon-field';
 
 const SimonFields = (props) => {
+  const { currentField, currentTurn } = props;
+
+  const renderField = (position) => {
+    return (
+      <SimonField
+        position={position}
+        currentField={currentField}
+        currentTurn={currentTurn}
+      />
+    );
+  };
+
   return (
     <div className="simon-fields column small-10 small-offset-1 text-center">
       <div className="row">
-        <SimonField
-          position="simon-top-left"
-        />
-        <SimonField
-          position="simon-top-right"
-        />
+        {renderField('simon-top-left')}
+        {renderField('simon-top-right')}
       </div>
       <div className="row">
-        <SimonField
-          position="simon-bottom-left"
-        />
-        <SimonField
-          position="simon-bottom-right"
-        />
+        {renderField('simon-bottom-left')}
+        {renderField('simon-bottom-right')}
       </div>
     </div>
   );
